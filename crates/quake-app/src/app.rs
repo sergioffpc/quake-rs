dcc8f4ecc3ddf58fed4f801596797d025e5b8808
Sérgio Carvalho <sergio.carvalho@miniclip.com>
2025-10-30 17:15:38 +0000

    feat(quake-renderer): add `quake-renderer` crate for rendering abstraction
    Introduce `quake-renderer` crate with `Renderer` struct for handling WGPU-based rendering pipeline. Integrate with `quake-app` and `quake-window` for window lifecycle and event management. Update workspace dependencies and modularize `quake-app` for rendering support.

--- crates/quake-app/src/app.rs
+++ crates/quake-app/src/app.rs
@@ -1,110 +1,39 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-use winit::application::ApplicationHandler;
-use winit::event::{KeyEvent, WindowEvent};
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::keyboard::PhysicalKey;
-use winit::window::{Window, WindowAttributes, WindowId};
+
+pub trait AppHandler {
+    fn on_created(&mut self);
+}
 
 pub fn run_app() -> anyhow::Result<()> {
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::new("resources/")?;
+    app.on_created();
 
-    let mut app = App::default();
-    event_loop.run_app(&mut app).map_err(|e| e.into())
+    quake_window::run_app(app)
 }
 
-#[derive(Default)]
 struct App {
-    inner: Option<InnerApp>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes = WindowAttributes::default()
-            .with_active(true)
-            .with_decorations(false)
-            .with_inner_size(winit::dpi::LogicalSize::new(2048, 1080))
-            .with_resizable(false)
-            .with_visible(true);
-        let window = event_loop.create_window(window_attributes).unwrap();
-
-        let mut inner = InnerApp::new(window).unwrap();
-        inner.register_builtin_commands();
-        inner.console.append_script("exec quake.rc");
-
-        self.inner = Some(inner);
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        if let Some(inner) = &mut self.inner {
-            inner.window_event(event_loop, window_id, event);
-        }
-    }
-}
-
-struct InnerApp {
-    window: Window,
     input: quake_input::Input,
     resources: Rc<RefCell<quake_resource::Resources>>,
+    renderer: Option<quake_renderer::Renderer>,
     console: quake_console::Console,
 }
 
-impl InnerApp {
-    fn new(window: Window) -> anyhow::Result<Self> {
+impl App {
+    fn new<P>(path: P) -> anyhow::Result<Self>
+    where
+        P: AsRef<std::path::Path>,
+    {
         let input = quake_input::Input::default();
-        let resources = Rc::new(RefCell::new(quake_resource::Resources::new("resources/")?));
+        let resources = Rc::new(RefCell::new(quake_resource::Resources::new(path)?));
         let console = quake_console::Console::new(resources.clone());
 
         Ok(Self {
-            window,
             input,
             resources,
+            renderer: None,
             console,
         })
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        if window_id != self.window.id() {
-            return;
-        }
-
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        physical_key: PhysicalKey::Code(key_code),
-                        text,
-                        ..
-                    },
-                ..
-            } => {
-                let key = match text {
-                    Some(ref key) => key.as_str(),
-                    None => &format!("{:?}", key_code),
-                };
-                if let Some(script) = self.input.on_key(&key) {
-                    self.console.append_script(&script);
-                }
-            }
-            WindowEvent::RedrawRequested => {
-                self.console.execute();
-
-                self.window.request_redraw();
-            }
-            _ => (),
-        }
     }
 
     fn register_builtin_commands(&mut self) {
@@ -117,5 +46,34 @@ impl InnerApp {
     fn register_quit_command(&mut self) {
         self.console
             .register_command("quit", move |_, _| std::process::exit(0));
+    }
+}
+
+impl AppHandler for App {
+    fn on_created(&mut self) {
+        self.register_builtin_commands();
+        self.console.append_script("exec quake.rc");
+    }
+}
+
+impl quake_window::WindowHandler for App {}
+
+impl quake_window::WindowLifecycleHandler for App {
+    fn on_created(&mut self, window: quake_window::WindowTarget) {
+        let renderer =
+            quake_renderer::Renderer::new(window.clone(), window.width(), window.height()).unwrap();
+        self.renderer = Some(renderer);
+    }
+}
+
+impl quake_window::WindowEventHandler for App {
+    fn on_key_pressed(&mut self, key: &str) {
+        if let Some(script) = self.input.on_key(&key) {
+            self.console.append_script(&script);
+        }
+    }
+
+    fn on_redraw_requested(&mut self) {
+        self.console.execute();
     }
 }
---
+++ crates/quake-renderer/src/lib.rs
@@ -0,0 +1,44 @@
+pub struct Renderer {
+    surface: wgpu::Surface<'static>,
+    surface_config: wgpu::SurfaceConfiguration,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+
+impl Renderer {
+    pub fn new<W>(target: W, width: u32, height: u32) -> anyhow::Result<Self>
+    where
+        W: raw_window_handle::HasWindowHandle
+            + raw_window_handle::HasDisplayHandle
+            + Send
+            + Sync
+            + 'static,
+    {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            flags: wgpu::InstanceFlags::DEBUG | wgpu::InstanceFlags::VALIDATION,
+            ..Default::default()
+        });
+        let surface = instance.create_surface(wgpu::SurfaceTarget::from(target))?;
+        let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        }))?;
+        let (device, queue) =
+            pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+                label: Some("Renderer device"),
+                ..Default::default()
+            }))?;
+        let surface_config = surface.get_default_config(&adapter, width, height).unwrap();
+        surface.configure(&device, &surface_config);
+
+        Ok(Self {
+            surface,
+            surface_config,
+            adapter,
+            device,
+            queue,
+        })
+    }
+}
